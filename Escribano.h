3c0a5d1a0d0ccadc60b3bd30e7b8f55978309b31
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2018-11-26 11:53:38 -0300

    Update Escribano.h
    Se le agrega el include de String

--- Escribano.h
+++ Escribano.h
@@ -1,7 +1,11 @@
 #ifndef ESCRIBANO_H_INCLUDED
 #define ESCRIBANO_H_INCLUDED
 
-typedef struct {String nombre;
-                         String apellido;}Escribano;
+#include "String.h"
+
+typedef struct {
+                String nombre;
+                String apellido;
+                }Escribano;
 
 #endif // ESCRIBANO_H_INCLUDED

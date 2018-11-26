c5060edce77bfb0e715037a3bba7a4de2b7b95f6
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2018-11-26 12:12:43 -0300

    Update Expediente.h
    Se agregan selectoras

--- Expediente.h
+++ Expediente.h
@@ -3,8 +3,15 @@
 #include "Escribano.h"
 
 typedef struct {int codigo;
-                          /*Caratula??*/
-                          Escribano esc;
-                          int cantPags;}Expediente;
+               /*Caratula??*/
+               Escribano esc;
+               int cantPags;
+               }Expediente;
+
+int Darcodigo(Expediente ex);
+
+Escribano DarEscrib(Expediente ex);
+
+int DarCantP(Expendiente ex);
 
 #endif // EXPEDIENTE_H_INCLUDED

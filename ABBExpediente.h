0eb80cd2e044b68d9e6d633571228e51ebc317d2
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2018-11-27 17:05:06 -0300

    Update ABBExpediente.h
    Se agregan las funciones Minimo, Borrar_Minimo y Borrar

--- ABBExpediente.h
+++ ABBExpediente.h
@@ -10,11 +10,28 @@ typedef struct nodoN{Expediente info;
 typedef nodo *Arbol;
 
 void InicializarArbol(Arbol &ABB);
+
 Boolean ExisteExp(int cod, Arbol abb);
+
 Boolean EsVacio(Arbol ABB);
+
 Expediente DarRaiz(Arbol ABB);
+
 Arbol HijoIzquierdo(Arbol ABB);
+
 Arbol HijoDerecho(Arbol ABB);
+
 void Cons(Arbol i, Arbol d, int valor, Arbol &ABB);
+
 void InsertarNodo(Arbol &ABB, Expediente ex);
+
+/* PRECONDICION: ABB no vacio */
+Expediente Minimo (Arbol ABB);
+
+/* Precondición : el árbol a NO está vacío */
+void Borrar_Minimo (Arbol &ABB);
+
+/* Precondición : el valor cod existe en el árbol a */
+void Borrar (Arbol &ABB, int cod);
+
 #endif // ABBEXPEDIENTE_H_INCLUDED

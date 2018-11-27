9833b227884d3ce972e199f2248e8e7ac9b37931
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2018-11-27 17:14:03 -0300

    Modulo de ListaRevision

---
+++ ListaRevision.cpp
@@ -0,0 +1,58 @@
+#include "stdafx.h"
+#include "ListaRevision.h"
+
+void Crear(ListRev &lis)
+{
+	lis = NULL;
+}
+Boolean Vacia(ListRev lis)
+{
+	Boolean es = FALSE;
+	if (lis == NULL)
+	es = TRUE;
+	return es;
+} 
+
+Revision Primero(ListRev lis)
+{
+	return lis -> info;
+}
+
+void Resto (ListRev &lis)
+{
+	ListRev aux = lis;
+	lis = lis -> sig;
+	delete aux;
+}
+
+void InsFront (ListRev &lis, Revision rev)
+{
+	ListRev aux = new Nodo;
+	aux -> info = rev;
+	aux -> sig = lis;
+	lis = aux;
+}
+
+Boolean EsValidaSis(ListRev lis, Fecha f)
+{
+	Boolean es = FALSE;
+	if(f.anio >= DarFecha(lis->info).anio && f.mes >=DarFecha(lis->info).mes && f.dia >= DarFecha(lis->info).dia)
+		es = TRUE;
+	return es;
+}
+
+void BorrarRev (ListRev &lis, int cod)
+{
+	if (lis != NULL)
+	{
+		if (cod == DarCodExpEnRev(lis -> info))
+		{
+			ListRev aux = lis;
+			lis = lis -> sig;
+			delete aux;
+			BorrarRev (lis, cod);
+		}
+	else
+		BorrarRev (lis -> sig, cod);
+	}
+} 
---
+++ ListaRevision.h
@@ -0,0 +1,22 @@
+#include "Revision.h"
+
+typedef struct nodo_rev{Revision info;
+						nodo_rev * sig;} Nodo;
+typedef Nodo * ListRev;
+
+void Crear(ListRev &lis);
+
+Boolean Vacia (ListRev lis);
+
+/* Precondición: lista NO vacía */
+Revision Primero(ListRev lis);
+
+/* Precondición: lista NO vacía */
+void Resto (ListRev &lis);
+
+void InsFront (ListRev &lis, Revision rev);
+
+//Precondicion: debe ser una fecha valida
+Boolean EsValidaSis(ListRev lis, Fecha f);
+
+void BorrarRev (ListRev &lis, int cod);

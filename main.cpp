3afd97bfd7af1052e9ea381f34aa775807720283
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2018-11-27 17:13:09 -0300

    Update main.cpp
    Se agregaron otros items del menu e inclusiones

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include "ABBExpediente.h"
+#include "ListaRevision.h"
 #include "Menu.h"
 
-using namespace std;
 
 int main()
 {
     Arbol ABB;
+	ListRev Lis;
     Expediente ex;
+	Revision rev;
     int cod, opcion;
+	Fecha f;
     InicializarArbol(ABB);
+	Crear(Lis);
+
     
     do
     {
@@ -26,9 +31,42 @@ int main()
                 }
                 else
                 {
-                    printf("El expediente ya existe!");
+                    printf("El expediente ya existe!");//Deberia volver al case 2?
                 }
             break;
+			case 2:
+				printf("Ingrese codigo del expediente: ");
+                scanf("%d", &cod);
+				if(ExisteExp(cod, ABB))
+                {
+					CargarFecha(f);
+					if(EsValidaSis(Lis, f)){
+						CrearRevision(rev, cod, f);
+						InsFront (Lis, rev);
+					}
+					else
+					{
+						printf("La fecha es menor a la de la ultima revisión ingresada");//Deberia volver al case 2?
+					}
+                }
+                else
+                {
+                    printf("El expediente no existe!");//Deberia volver al case 2?
+                }
+			break;
+			case 3:
+				printf("Ingrese codigo del expediente: ");
+                scanf("%d", &cod);
+				if(ExisteExp(cod, ABB))
+                {
+					Borrar (ABB, cod);
+					BorrarRev(Lis, cod);
+                }
+                else
+                {
+                    printf("El expediente no existe!");//Deberia volver al case 3?
+                }
+			break;
         }
 
     }while(opcion != 0);

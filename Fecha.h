041faa45780dabaaa73a7520d43224518247b2fa
mathiaspouso18 <mathiaspouso18@gmail.com>
2018-11-27 11:38:52 -0300

    Update Fecha.h

--- Fecha.h
+++ Fecha.h
@@ -9,5 +9,6 @@ int DarDia(Fecha f);
 int DarMes(Fecha f);
 int DarAnio(Fecha f);
 void CargarFecha(Fecha &f);
-
+void DesplegarFecha(Fecha f);
+Boolean EsValida(Fecha f);
 #endif // FECHA_H_INCLUDED

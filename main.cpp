f15bf97e88dcda39f6871dac01f4792c1fe99202
mathiaspouso18 <mathiaspouso18@gmail.com>
2018-11-27 12:03:39 -0300

    Update main.cpp
    Se agrega el ver el menu desde main.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "ABBExpediente.h"
+#include "Menu.h"
 
 using namespace std;
 
@@ -6,20 +7,31 @@ int main()
 {
     Arbol ABB;
     Expediente ex;
-    int cod;
+    int cod, opcion;
     InicializarArbol(ABB);
-
-    printf("Ingrese codigo del expediente: ");
-    scanf("%d", &cod);
-    if(!ExisteExp(cod, ABB))
+    
+    do
     {
-        CrearExpediente(ex, cod);
-        InsertarNodo(ABB, ex);
-    }
-    else
-    {
-        printf("El expediente ya existe!");
-    }
+        MenuPrincipal(opcion);
 
-    //Agregar la creacion de la revision. Validar fecha y codigo.
+        switch(opcion)
+        {
+            case 1:
+                printf("Ingrese codigo del expediente: ");
+                scanf("%d", &cod);
+                if(!ExisteExp(cod, ABB))
+                {
+                    CrearExpediente(ex, cod);
+                    InsertarNodo(ABB, ex);
+                }
+                else
+                {
+                    printf("El expediente ya existe!");
+                }
+            break;
+        }
+
+    }while(opcion != 0);
+
+    opcion = -1;//limpio la variable dejandola en -1;
 }

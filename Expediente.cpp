96aa479825d7a08167ac1eeac21903bb17494b29
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2018-11-29 17:16:30 -0300

    Update Expediente.cpp
    Consistencia en archivos

--- Expediente.cpp
+++ Expediente.cpp
@@ -35,3 +35,27 @@ void DarApellidoEscrib(Expediente ex, String &apellido)
 int DarCantP(Expediente ex){
 	return ex.cantPags;
 }
+
+void Bajar_Expediente (Expediente ex, FILE * f) 
+{
+	String nom, ape;
+	fwrite (&ex.codigo, sizeof(int), 1, f);
+	Bajar_String (ex.Caratula, f);
+	DarNombreEscrib(ex,nom);
+	DarApellidoEscrib(ex,ape);
+	Bajar_String (nom, f);
+	Bajar_String (ape, f);
+	fwrite (&ex.cantPags, sizeof(int), 1, f);
+} 
+
+void Levantar_Expediente (Expediente &ex, FILE * f)
+{
+	Escribano esc;
+	fread(&ex.codigo,sizeof(int), 1, f);
+	strcrear (ex.Caratula);
+	Levantar_String (ex.Caratula, f);
+	Levantar_String (esc.nombre, f);
+	Levantar_String (esc.apellido, f);
+	ex.esc = esc;
+	fread (&ex.cantPags, sizeof(int), 1, f);
+} 

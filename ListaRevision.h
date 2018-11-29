e921ae3abc8869d74fc9fecbc65f591aeb625c49
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2018-11-29 17:13:38 -0300

    Update ListaRevision.h
    Se agregaron reportes y consistencia en archivos

--- ListaRevision.h
+++ ListaRevision.h
@@ -32,3 +32,10 @@ Boolean TieneRev (ListRev lis, int cod);
 
 /*Precondicion: el expediente debe existir en el sistema y debe tener al menos una revision*/
 void ListarRevxExp(ListRev lis, int cod);
+
+/* Precondición: lista NO vacía */
+void RevxEvaluacion(ListRev lis, int &Satis, int &inComp, int &Pend);
+
+void Bajar_Lista (ListRev lis , String nomArch);
+
+void Levantar_Lista (ListRev &lis, String nomArch);
